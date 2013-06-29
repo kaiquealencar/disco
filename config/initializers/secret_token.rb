1585af3b04b036d78ee9c201c8a33b55367a3080
Kaique Alencar <kaique.alencar@gmail.com>
2013-06-29 08:16:34 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disco::Application.config.secret_token = 'b07411ecdfb823513f6af4dfc9f7b16e1f15071ee4879ec011b6d01841c3b1597e713ff9d7706be7ef4a7bbb4f1a9bcf420c8f90879ae3e0644b245c141155c7'
